d8628ec9918114d83129dfd1f21b2e0f72dd6325
hemal <hemal@ubuntu-17.04>
2018-08-19 12:31:47 +0530

    holidays ui bettered

--- holiday/models.py
+++ holiday/models.py
@@ -3,10 +3,10 @@ from makemyholiday.settings import STATIC_URL
 # Create your models here.
 
 class Holidays(models.Model):
+    name = models.CharField(max_length = 100)
     photo = models.CharField(max_length = 100)
     description = models.TextField()
     price = models.FloatField()
     highlights = models.TextField()
     category = models.CharField(max_length = 15)
-    days = models.IntegerField() 
-
+    days = models.IntegerField()
